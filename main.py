f06735a36336394f3c7a59f216cf4efb10adeecb
harryandrus windows <56275662+HarryAndrus@users.noreply.github.com>
2024-08-04 13:45:49 -0700

    deal cards

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import secrets
+import os
 
 
 def create_deck() -> list:
@@ -14,13 +15,18 @@ def create_deck() -> list:
     return deck
 
 
-def getplayers() -> list:
+def shuffle(deck: list) -> list:
+    return sorted(deck, key=lambda _: secrets.randbelow(100))
+
+
+def getplayers() -> int:
     while True:
         try:
             num_players = int(input("Enter the number of players (1-8): "))
             if 1 <= num_players <= 8:
                 break
             else:
+                os.system('cls' if os.name == 'nt' else 'clear')
                 print("Please enter a number between 1 and 8.")
         except ValueError:
             print("Please enter a valid number.")
@@ -28,9 +34,23 @@ def getplayers() -> list:
     return num_players
 
 
-def deal_cards(num_players: int):
-
+def deal_cards(num_players: int, shuffled_deck: list):
+    community_cards = []
     player_cards = [[] for _ in range(num_players)]
+    for _ in range(2):
+        for player in player_cards:
+            player.append(shuffled_deck.pop())
+
+    shuffled_deck.pop()
+
+    for _ in range(5):
+        community_cards.append(shuffled_deck.pop())
+
+    print(f"Community Cards: {community_cards}")
+
+
+    for i, cards in enumerate(player_cards, start=1):
+        print(f"Player {i}: {cards}")
 
 
 def output() -> None:
@@ -38,11 +58,12 @@ def output() -> None:
 
 
 def main():
-    deck = create_deck()
-    shuffled_deck = sorted(deck, key=lambda _: secrets.randbelow(100))
+    deck: list = create_deck()
+    shuffled_deck: list = shuffle(deck)
 
-    numPlayers = getplayers()
-    print(numPlayers)
+    numPlayers: int = getplayers()
+    deal_cards(numPlayers, shuffled_deck)
+
     # print("\n".join(shuffled_deck))
 
 
